afc68752b12fd72903fe805e479fdd1b80e918ec
Christopher Aytona <christopher.aytona@gmail.com>
2016-03-29 11:07:00 -0400

    Added playerstats script

---
+++ Assets/Scripts/PlayerStats.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerStats : MonoBehaviour {
+
+	public string attribName;
+	public int attribAmount;
+
+	public PlayerStats(string name, int amount) {
+		attribName = name;
+		attribAmount = amount;
+	}
+
+	// Set default attrib amount to 1
+	public int CompareTo(PlayerStats other) {
+		if (other == null)
+			return 1;
+		return attribAmount - other.attribAmount;
+	}
+}
